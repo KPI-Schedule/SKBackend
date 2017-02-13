d2f634c4c0471f5f5741cfb821b29126d7cc9dcc
DCapriCorn <dc.avoru@gmail.com>
2017-02-13 20:30:15 +0200

    migrations added

--- database/migrations/2017_02_13_181158_insertGroupsTable.php
+++ database/migrations/2017_02_13_181158_insertGroupsTable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class InsertGroupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at')->nullable();
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('en');
+            $table->string('ru');
+            $table->string('ukr');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('groups');
     }
 }
